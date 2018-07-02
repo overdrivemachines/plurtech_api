e8f68b175672beb774b3eee47ad8822f876c4745
Dipen Chauhan <get.dipen@gmail.com>
2018-07-02 00:23:53 -0700

    Annotated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  username   :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+	validates_uniqueness_of :email, :username
 end
