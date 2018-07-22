6bc7cdc10189fbc222a703e6dadad4725f83b091
Dipen Chauhan <get.dipen@gmail.com>
2018-07-22 15:57:57 -0700

    Removed users

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_065228) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "username"
-    t.string "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
