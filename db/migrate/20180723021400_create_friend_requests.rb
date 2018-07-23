5fbb2359203ac896689e07c7f37567505aab452a
Dipen Chauhan <get.dipen@gmail.com>
2018-07-23 14:26:34 -0700

    Fixed SQLite3::SQLException: no such table

--- db/migrate/20180723021400_create_friend_requests.rb
+++ db/migrate/20180723021400_create_friend_requests.rb
@@ -2,13 +2,11 @@ class CreateFriendRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :friend_requests do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :friend, foreign_key: false
 
       t.timestamps
     end
 
-    add_index :friend_requests, :user_id
-    add_index :friend_requests, :friend_id
-    add_index :friend_requests, [:user_id, :friend_id], unique: true
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
--- db/migrate/20180723022812_create_friendships.rb
+++ db/migrate/20180723022812_create_friendships.rb
@@ -2,13 +2,11 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.references :friend, foreign_key: false
 
       t.timestamps
     end
 
-    add_index :friendships, :user_id
-    add_index :friendships, :friend_id
-    add_index :friendships, [:user_id, :friend_id], unique: true
+    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
