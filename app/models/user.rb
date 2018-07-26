74d72c0c345112ad7a738b7d615ea2583b23c80f
Dipen Chauhan <get.dipen@gmail.com>
2018-07-26 02:01:07 -0700

    Added methods for different tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 	has_many :incoming_friend_requests, class_name: "FriendRequest", foreign_key: "friend_id"
 	
 	has_many :friendships, dependent: :destroy
-	has_many :friends, through: :friendships
+	has_many :friends, class_name: "Friendship", foreign_key: "friend_id"
 
 	def send_friend_request(f)
 		self.friend_requests.create(friend_id: f.id)
@@ -30,6 +30,35 @@ class User < ApplicationRecord
 		# TODO
 	end
 	def remove_friend(friend)
-		current_user.friends.destroy(friend)
+		self.friends.destroy(friend)
+	end
+
+	# Returns an array of user id's that friend requested you
+	def list_incoming_friend_requests
+		self.incoming_friend_requests.pluck(:user_id)
+	end
+
+	# Returns an array of user id's that you sent friend requests to
+	def list_outgoing_friend_requests
+	  self.friend_requests.pluck(:friend_id)
+	end
+
+	# Returns an array of user id's of your friends
+	def list_friends
+	  self.friendships.pluck(:friend_id)
+	end
+
+	# Accepts friend request from friend f
+	def accept_friend_request(fid)
+		if list_incoming_friend_requests.include?(fid)
+			# TODO
+			return true
+		else
+			return false
+		end
+	end
+
+	# Denies friend request from friend f
+	def deny_friend_request(fid)
 	end
 end
