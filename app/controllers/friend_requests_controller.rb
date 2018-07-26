3437cd98bb12e5f7b0f0fa3e3b4d73715f8af7c4
Dipen Chauhan <get.dipen@gmail.com>
2018-07-26 02:00:03 -0700

    Showing incoming and outgoing friend requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,15 +1,22 @@
 class FriendRequestsController < ApplicationController
   before_action :set_friend_request, except: [:index, :create]
+  before_action :set_user
 
   def index
     # TODO: json
-    @incoming = FriendRequest.where(friend: current_user)
-    @outgoing = current_user.friend_requests
+    @incoming = FriendRequest.where(friend: @user).pluck(:user_id)
+    @outgoing = @user.friend_requests.pluck(:friend_id)
+
+    render json: {
+      incoming: @incoming,
+      outgoing: @outgoing
+    }.to_json
+
   end
 
   def create
     friend = User.find(params[:friend_id])
-    @friend_request = current_user.friend_requests.new(friend: friend)
+    @friend_request = @user.friend_requests.new(friend: friend)
 
     if @friend_request.save
       # TODO: json
@@ -34,4 +41,9 @@ class FriendRequestsController < ApplicationController
   def set_friend_request
     @friend_request = FriendRequest.find(params[:id])
   end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
