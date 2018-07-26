d75c035d777821fef6c841d7457d4f884798edc8
Dipen Chauhan <get.dipen@gmail.com>
2018-07-26 02:01:44 -0700

    Displaying friend ids onluy

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   before_action :set_user
   before_action :set_friend, only: :destroy
   def index
-    @friends = @user.friends
+    @friends = @user.friendships.pluck(:friend_id) #Friendship.where(user_id: @user.id)
     render json: @friends
   end
 
