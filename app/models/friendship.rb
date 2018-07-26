3b270d6b3f8a09acf91e5d7d4ea9cb5ff2ecbbb5
Dipen Chauhan <get.dipen@gmail.com>
2018-07-25 17:16:32 -0700

    Added validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -10,15 +10,32 @@
 #
 
 class Friendship < ApplicationRecord
-  after_create :create_inverse_relationship
-  after_destroy :destroy_inverse_relationship
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  
+  # User needs to be present
   validates :user, presence: true
+  # Friend needs to be present and prevent repeats of the same row
+  # f = Friend.create(user_id: 2, friend_id: 3)
+  #   commit transaction
+  # f = Friend.create(user_id: 2, friend_id: 3)
+  #   rollback transaction
+  # f.errors.full_messages
+  #  => ["Friend has already been taken"]
   validates :friend, presence: true, uniqueness: { scope: :user }
-  validate :not_self
+  validate :not_self # Don't allow a user to friend themselves
+
+  after_create :create_inverse_relationship
+  after_destroy :destroy_inverse_relationship
 
   private
+
+  # Don't allow a user to friend themselves
+  def not_self
+    if user_id == friend_id
+      errors.add(:friend, "can't be equal to user") 
+    end
+  end
 
   def create_inverse_relationship
     friend.friendships.create(friend: user)
@@ -27,10 +44,5 @@ class Friendship < ApplicationRecord
   def destroy_inverse_relationship
     friendship = friend.friendships.find_by(friend: user)
     friendship.destroy if friendship
-  end
-
-  # user won’t be able to befriend himself
-  def not_self
-    errors.add(:friend, "can't be equal to user") if user == friend
   end
 end
