355ed06e7404a65ee344dfdd819354c237592233
Dipen Chauhan <get.dipen@gmail.com>
2018-07-31 10:54:27 -0700

    Finished destroy friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,23 +1,34 @@
 class FriendsController < ApplicationController
-  before_action :set_user
+  before_action :set_user, only: :index
   before_action :set_friend, only: :destroy
+  
+  # GET    /users/:user_id/friends
   def index
     @friends = @user.friendships.pluck(:friend_id) #Friendship.where(user_id: @user.id)
     render json: @friends
   end
 
+  
+  # DELETE /users/:user_id/friends/:id
   def destroy
-    @user.remove_friend(@friend)
+    # @user.remove_friend(@friend)
+    @user.friends.destroy(@friend)
     head :no_content
   end
 
   private
 
   def set_friend
-    @friend = @user.friends.find(params[:id])
+    @friend = @user.friends.find_by_id(params[:id])
+    if (@friend.nil?)
+      head :not_found
+    end
   end
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = User.find_by_id(params[:user_id])
+    if (@user.nil?)
+      head :not_found
+    end
   end
 end
