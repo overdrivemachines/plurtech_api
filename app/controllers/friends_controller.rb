9cf091a61105c61e386bac590998602f234d4f10
Dipen Chauhan <get.dipen@gmail.com>
2018-07-31 14:23:56 -0700

    Optimized and fixed destroy

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,5 @@
 class FriendsController < ApplicationController
   before_action :set_user, only: :index
-  before_action :set_friend, only: :destroy
   
   # GET    /users/:user_id/friends
   def index
@@ -10,21 +9,26 @@ class FriendsController < ApplicationController
 
   
   # DELETE /users/:user_id/friends/:id
+  # :id is the id of friend not the Friendship record's id
   def destroy
-    # @user.remove_friend(@friend)
-    @user.friends.destroy(@friend)
-    head :no_content
+    # Check if @user and @friend are really friends
+    f = Friendship.find_by_user_id_and_friend_id(params[:user_id], params[:id])
+    if f.nil?
+      head :not_found
+    else
+      f.destroy
+      head :no_content
+    end    
   end
 
   private
 
-  def set_friend
-    set_user
-    @friend = @user.friends.find_by_user_id(params[:id])
-    if (@friend.nil?)
-      head :not_found
-    end
-  end
+  # def set_friend
+  #   @friend = User.find_by_id(params[:id])
+  #   if (@friend.nil?)
+  #     head :not_found
+  #   end
+  # end
 
   def set_user
     @user = User.find_by_id(params[:user_id])
